3ee4bebd375ad1203e9685c66775b092500b84cf
OYM-DEV7 <oymdev7@oym.com.py>
2018-10-31 10:37:02 -0300

    julio-webpage con heroku

--- 01- hola mundo/app.js
+++
@@ -1,7 +0,0 @@
-let nombre = 'César';
-//console.log( 'Hola' + nombre );
-console.log(`Hola ${nombre}`);
-let a = 10;
-let b = 20;
-
-console.log(a + b);
--- 05-clima-mundo/app2.js
+++
@@ -1,2 +0,0 @@
-let mapa = require('mapbox-gl/dist/mapbox-gl.js');
-let mymap = L.map('mapid').setView([51.505, -0.09], 13);
